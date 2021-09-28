4df2e740193a9e389adff2736237f9674540094d
Tobias Christian <tcschenk@github.com>
2021-09-28 19:43:21 +0200

    Build the first API

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -1,14 +1,76 @@
-
+using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CoreCodeCamp.Controllers
 {
     [Route("api/[Controller]")]
     public class CampsController : ControllerBase
     {
+        private readonly ICampRepository repository;
+        private readonly IMapper mapper;
+
+        public CampsController(ICampRepository repository, IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
         [HttpGet]
-        public IActionResult Get() {
-            return Ok(new { Moniker = "ATL2018", Name = "Atlanta Code Camp" });
+        public async Task<ActionResult<CampModel[]>> Get(bool includeTalks = false)
+        {
+            try
+            {
+                var results = await repository.GetAllCampsAsync(includeTalks);
+
+                return mapper.Map<CampModel[]>(results);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        [HttpGet("{moniker}")]
+        public async Task<ActionResult<CampModel>> Get(string moniker)
+        {
+            try
+            {
+                var results = await repository.GetCampAsync(moniker);
+
+                if (results == null)
+                {
+                    return NotFound();
+                }
+
+                return mapper.Map<CampModel>(results);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        [HttpGet("search")]
+        public async Task<ActionResult<CampModel[]>> SearchByDate(DateTime date, bool includeTalks = false)
+        {
+            try
+            {
+                var result = await repository.GetAllCampsByEventDate(date, includeTalks);
+
+                if (!result.Any()) return NotFound();
+
+                return mapper.Map<CampModel[]>(result);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
         }
     }
 }
--- src/Startup.cs
+++ src/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using CoreCodeCamp.Data;
 using Microsoft.AspNetCore.Builder;
@@ -14,32 +15,33 @@ using Microsoft.Extensions.Options;
 
 namespace CoreCodeCamp
 {
-  public class Startup
-  {
-    public void ConfigureServices(IServiceCollection services)
+    public class Startup
     {
-      services.AddDbContext<CampContext>();
-      services.AddScoped<ICampRepository, CampRepository>();
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CampContext>();
+            services.AddScoped<ICampRepository, CampRepository>();
 
-      services.AddControllers();
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllers();
+            });
+        }
     }
-
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-
-      app.UseRouting();
-
-      app.UseAuthentication();
-      app.UseAuthorization();
-
-      app.UseEndpoints(cfg =>
-      {
-        cfg.MapControllers();
-      });
-    }
-  }
 }
